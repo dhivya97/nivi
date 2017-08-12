0e6ffc1cd371568b1bf3abfcef6ef81448b79241
dhivya97 <30342650+dhivya97@users.noreply.github.com>
2017-08-12 16:46:20 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int getnumber();    
+int main() 
+{
+int input = 0;
+input = getnum();
+while (!((input <= 9) && (input >= 1))) {
+printf("[ERROR] The number you entered is out of range");
+ input = getnum();
+    }
+  printf("The number you entered is %d", input);
+    return 0;
+}      
